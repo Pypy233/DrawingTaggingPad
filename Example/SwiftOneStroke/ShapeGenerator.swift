94dfc95e24883574a7de8acf4c19f013805d1d86
2529716798 <2529716798@qq.com>
2018-09-16 01:59:18 +0800

    finish generator class

--- Example/SwiftOneStroke/ShapeGenerator.swift
+++ Example/SwiftOneStroke/ShapeGenerator.swift
@@ -19,7 +19,48 @@ class ShapeGenerator {
         return StrokePoint(x: x, y: y)
     }
     
+    func getPermiter(shapeType: ShapeView.ShapeType) -> [Double] {
+        let xLeastPoint = getStrokeBounds()[0]
+        let xMostPoint  = getStrokeBounds()[1]
+        let yLestPoint  = getStrokeBounds()[2]
+        let yMostPoint  = getStrokeBounds()[3]
+        
+        var permiterArray = [Double]()
+        
+        switch shapeType {
+        case .Circle:
+            permiterArray.append(getAveragePermiter(point1: xLeastPoint, point2: xMostPoint, point3: yLestPoint, point4: yMostPoint))
+            return permiterArray
+            
+        case .Rectangle:
+            let width =  getAveragePermiter(point1: yLestPoint, point2: xMostPoint, point3: xLeastPoint, point4: yMostPoint)
+            let height = getAveragePermiter(point1: xLeastPoint, point2: yLestPoint, point3: xMostPoint, point4: yMostPoint)
+            permiterArray.append(width)
+            permiterArray.append(height)
+            return permiterArray
+            
+        case .Triangle:
+            permiterArray.append(xLeastPoint.distanceTo(xMostPoint))
+            permiterArray.append(yLestPoint.distanceTo(yMostPoint))
+            permiterArray.append(getAveragePermiter(point1: yMostPoint, point2: xLeastPoint, point3: yMostPoint, point4: xMostPoint))
+            return permiterArray
+            
+        default:
+            let width =  getAveragePermiter(point1: yLestPoint, point2: xMostPoint, point3: xLeastPoint, point4: yMostPoint)
+            let height = getAveragePermiter(point1: xLeastPoint, point2: yLestPoint, point3: xMostPoint, point4: yMostPoint)
+            permiterArray.append((width + height) / 2)
+            return permiterArray
+        }
+    }
     
+    
+    /** 
+     两边取平均
+     **/
+    func getAveragePermiter(point1: StrokePoint, point2
+        : StrokePoint, point3: StrokePoint, point4: StrokePoint) -> Double {
+        return (point1.distanceTo(point2) + point3.distanceTo(point4)) / 2
+    }
     
     /**
      计算初始点到结束点的距离
